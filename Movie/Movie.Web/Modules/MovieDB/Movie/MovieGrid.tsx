d1b5f594f93878174862510920ba5240e1592e0e
HakmElkady <ENGHAKM24@GMAIL.COM>
2024-10-28 22:38:36 +0300

    Adding a Movie Kind Field

--- Movie/Movie.Web/Modules/MovieDB/Movie/MovieGrid.tsx
+++ Movie/Movie.Web/Modules/MovieDB/Movie/MovieGrid.tsx
@@ -1,4 +1,4 @@
-﻿import { Decorators, EntityGrid } from '@serenity-is/corelib';
+import { Decorators, EntityGrid, localText, QuickSearchField   } from '@serenity-is/corelib';
 import { MovieColumns, MovieRow, MovieService } from '../../ServerTypes/MovieDB';
 import { MovieDialog } from './MovieDialog';
 
@@ -8,4 +8,15 @@ export class MovieGrid extends EntityGrid<MovieRow> {
     protected getDialogType() { return MovieDialog; }
     protected getRowDefinition() { return MovieRow; }
     protected getService() { return MovieService.baseUrl; }
+
+    protected getQuickSearchFields(): QuickSearchField[] {
+        const txt = s => localText(`Db.${MovieRow.localTextPrefix}.${s}`);
+        const fld = MovieRow.Fields;
+        return [
+            { name: "", title: "All" },
+            { name: fld.Description, title: txt(fld.Description) },
+            { name: fld.Storyline, title: txt(fld.Storyline) },
+            { name: fld.Year, title: txt(fld.Year) }
+        ];
+    }
 }
