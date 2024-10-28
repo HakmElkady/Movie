b4b622fe5cbc9ae14cb9f9f2c6175d941dd7f72d
HakmElkady <ENGHAKM24@GMAIL.COM>
2024-10-28 22:08:30 +0300

    Handling Movie Navigation

--- Movie/Movie.Web/Modules/MovieDB/Movie/MovieForm.cs
+++ Movie/Movie.Web/Modules/MovieDB/Movie/MovieForm.cs
@@ -1,4 +1,4 @@
-﻿using Serenity.ComponentModel;
+using Serenity.ComponentModel;
 using System;
 
 namespace Movie.MovieDB.Forms;
@@ -8,9 +8,15 @@ namespace Movie.MovieDB.Forms;
 public class MovieForm
 {
     public string Title { get; set; }
+
+    [TextAreaEditor(Rows = 3)]
     public string Description { get; set; }
+
+    [TextAreaEditor(Rows = 8)]
     public string Storyline { get; set; }
     public int Year { get; set; }
     public DateTime ReleaseDate { get; set; }
     public int Runtime { get; set; }
+
+
 }
